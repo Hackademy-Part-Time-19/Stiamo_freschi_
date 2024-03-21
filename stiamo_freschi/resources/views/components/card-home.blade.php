a0681f770b6d39210647fd333e1e1b32930e78c4
Lleanza7 <lucaleanza92@hotmail.com>
2024-03-21 18:45:12 +0100

    sistemazione card

--- stiamo_freschi/resources/views/components/card-home.blade.php
+++ stiamo_freschi/resources/views/components/card-home.blade.php
@@ -1,10 +1,10 @@
 <div class="cardLayout">
     <div class="imgCard">
-
+        <img src="https://images1.vinted.net/t/02_0126f_7UxTSDC7QPA9b5SwtpjvXi9K/f800/1710953115.jpeg?s=2b251215e7d94314a9ffa97432a4b9413faa19e3" alt="">
     </div>
     <div class="userCard">
         <div class="imageUser">
-
+            <img src="https://banner2.cleanpng.com/20180508/toe/kisspng-user-profile-computer-icons-clip-art-5af1ac8cee74c6.8111281615257877889767.jpg" alt="">
         </div>
         <div class="nomeUser">
             <p>Luca Leanza</p>
@@ -19,4 +19,6 @@
         <p>Descrizione di prova</p>
         <button class="btnCardLayout">Vai al dettaglio</button>
     </div>
+
+    
 </div>
