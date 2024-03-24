fd4460222a6e30da6929c5905bd8688f4f2b7e34
fabio <fabio.forenza@gmail.com>
2024-03-24 12:24:15 +0100

    nav

--- stiamo_freschi/resources/views/components/navbar.blade.php
+++ stiamo_freschi/resources/views/components/navbar.blade.php
@@ -153,12 +153,32 @@
         @endguest
         @auth
             <div class="div-btn-desk">
-                <a href="/dashboard"><button class="btn-desk2">Utente logato : <span style="font-weight: bold">
-                            {{ auth()->user()->name }}</span></button></a>
-                <form action="/logout" method="post">
-                    @csrf
-                    <button class="btn-desk2 logout">LOGOUT</button>
-                </form>
+                <div style="width: 50%;">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
+                        </button>
+                        <ul class="dropdown-menu index" aria-labelledby="dropdownMenuButton">
+                            <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                            <li><a class="dropdown-item" href="#">Opzione 2</a></li>
+                            <li><a class="dropdown-item" href="#">
+                                    <form action="/logout" method="post">
+                                        @csrf
+                                        <button class="btn-desk2 logout">LOGOUT</button>
+                                    </form>
+                                </a>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         @endauth
     </div>
