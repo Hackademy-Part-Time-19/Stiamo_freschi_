bb5e7aceba51405ca5f036b0b945df58ef4f32a1
fabio <fabio.forenza@gmail.com>
2024-03-25 22:18:06 +0100

    sistemazione

--- stiamo_freschi/resources/views/dashboard-revisore.blade.php
+++ stiamo_freschi/resources/views/dashboard-revisore.blade.php
@@ -13,21 +13,11 @@
                 <li>Basket</li>
             </ul>
         </div>
-
-
-
-
-
-
-
         <div style="flex: 2;background-color:blueviolet;margin-top:60px;display:flex">
             <div style="flex: 2">
                 <h1>Good day, artem</h1>
                 <p>we are collecting articoles for you </p>
                 <h2> Notes </h2>
-
-
-
                 <div style="display: flex;width:100%;justify-content:center;align-items:center;padding:30px;">
                     <div class="row" style="--bs-gutter-x: 0rem;">
 
@@ -55,17 +45,13 @@
                                 <p>Descrizione {{-- $description --}}</p>
 
                             </div>
-
                             <div class="Areabutton">
 
                                 <button class="btnCardLayout"><a style="color: white" href="{{-- route('announcement.show', ['announcement' => $announcement]) --}}">Vai
                                         al
                                         dettaglio</a></button>
                             </div>
-
-
                         </div>
-
                         <div class="cardLayout box col-sm-4" style="margin: 10px">
                             <div class="imgCard">
                                 <img src="https://images1.vinted.net/t/02_0126f_7UxTSDC7QPA9b5SwtpjvXi9K/f800/1710953115.jpeg?s=2b251215e7d94314a9ffa97432a4b9413faa19e3"
@@ -178,41 +164,33 @@
                 <div style="display: flex; align items:center; justify-content:center">
                     <div class="row" style="--bs-gutter-x: 0rem;" ;>
                         <div class="col-sm-4 mb-3 mb-sm-0" style="margin: 10px">
-                          <div class="card" style="height: 120px; width:220px">
-                            <div class="card-body" style="margin: 0; width:100%;height:100%;background-color:white;display:block">
-                              <h5 class="card-title">Special title treatment</h5>
-                              
-                              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                             
+                            <div class="card" style="height: 120px; width:220px">
+                                <div class="card-body"
+                                    style="margin: 0; width:100%;height:100%;background-color:white;display:block">
+                                    <h5 class="card-title">Special title treatment</h5>
+
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional
+                                        content.</p>
+
+                                </div>
                             </div>
-                          </div>
                         </div>
                         <div class="col-sm-4 mb-3 mb-sm-0 " style="margin: 10px">
                             <div class="card" style="height: 120px; width:220px">
-                              <div class="card-body" style="margin: 0; width:100%;height:100%;background-color:white;display:block">
-                                <h5 class="card-title">Special title treatment</h5>
-                                
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                               
-                              </div>
-                            </div>
-                          </div>
-                      </div>
-                    
-                </div>
+                                <div class="card-body"
+                                    style="margin: 0; width:100%;height:100%;background-color:white;display:block">
+                                    <h5 class="card-title">Special title treatment</h5>
 
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional
+                                        content.
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-
-
-
-
     </div>
-
-
-
-
-
-
 </x-layout>
