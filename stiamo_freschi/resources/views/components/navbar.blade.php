bde576c1eea9b12acd1cb7c7ac4aa02a7bf6d3f2
fabio <fabio.forenza@gmail.com>
2024-03-25 18:41:24 +0100

    colore

--- stiamo_freschi/resources/views/components/navbar.blade.php
+++ stiamo_freschi/resources/views/components/navbar.blade.php
@@ -128,8 +128,7 @@
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" style="margin-top: 13px; height:35px" type="search"
                         placeholder="Cerca il tuo annuncio" aria-label="Search">
-                    <button class="btn btn-outline-secondary btn-search"
-                        style="color: white; background-color: #081F37;height:35px;" type="submit">Cerca</button>
+                    <button class="btn btn-outline-secondary btn-search" style="" type="submit">Cerca</button>
                 </form>
                 <div class="btn-ann" onclick="scrollFunction()" id="scrollToTopBtn">
                     <a href="{{ route('announcement.create') }}"><button class="btn-desk2">Inserisci articolo</button></a>
@@ -140,8 +139,7 @@
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" style="margin-top: 13px; height:35px" type="search"
                         placeholder="Cerca il tuo annuncio" aria-label="Search">
-                    <button class="btn btn-outline-secondary btn-search"
-                        style="color: white; background-color: #081F37;height:35px;" type="submit">Cerca</button>
+                    <button class="btn btn-outline-secondary btn-search" type="submit">Cerca</button>
                 </form>
             </div>
         @endauth
@@ -174,7 +172,7 @@
                             d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
                     </svg>
                 </div>
-                <div class="dropdown" style="flex:1">
+                <div class="dropdown icon-btn" style="flex:1">
                     <button class="btn dropdown-toggle icon-profile" type="button" id="dropdownMenuButton"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
