e4782b8bd096fa6df737df7276f1edb4539462a0
fabio <fabio.forenza@gmail.com>
2024-03-27 21:32:30 +0100

    dashboard

---
+++ stiamo_freschi/resources/views/components/layout-revisore.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Akshar:wght@300..700&family=Encode+Sans+Condensed&family=News+Cycle&family=Pontano+Sans:wght@300..700&display=swap"
+        rel="stylesheet">
+
+    @vite (['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    {{ $slot }}
+
+
+
+</body>
+
+</html>
--- stiamo_freschi/resources/views/dashboard-revisore.blade.php
+++ stiamo_freschi/resources/views/dashboard-revisore.blade.php
@@ -1,7 +1,6 @@
-<x-layout>
+<x-layout-revisore>
     <x-navbar />
-    <div style="display: flex;">
-
+    <div style="display: flex;height:max-content">
         <div class="aside-left">
             <ul class="ul-aside">
                 <div class="li-aside">
@@ -35,15 +34,17 @@
             </div>
             <div class="card-dash">
                 <div class="inserzione">
-                    <div style="display: flex;  width: 80%;padding:10px;">
-                        <button style="flex:0.2; margin:10px" onclick="">
+                    <div
+                        style="display: flex;  width: 80%;padding:10px;justify-content: center;
+                    align-items: center;">
+                        <button class="btn4 true" style="flex:0.2; margin:10px" onclick="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="currentColor"
                                 class="bi bi-check2" viewBox="0 0 16 16">
                                 <path
                                     d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
                             </svg>
                         </button>
-                        <button style="flex:0.2;margin:10px" onclick="">
+                        <button class="btn4 false" style="flex:0.2;margin:10px" onclick="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                                 class="bi bi-x-lg" viewBox="0 0 16 16">
                                 <path
@@ -51,19 +52,33 @@
                             </svg>
                         </button>
                         <div style="flex:0.5;margin:10px; border-right:1px solid black ">
-                            <h3 style="padding: 0px">Annunci</h3>
+                            <h3 style="padding: 0px">Titolo</h3>
                         </div>
-                        <div style="flex:1;margin:10px; margin-right:50px">
-                            <h3>Vestito</h3>
+                        <div style="flex:1;margin:10px; margin-right:50px;">
+                            <div style="margin-left: 20px">
+                                <h3>Categoria</h3>
+                            </div>
                         </div>
                     </div>
-                    <button style="margin:30px; position: relative;right:5px" onclick="apriBox()">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                            class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                        </svg>
-                    </button>
+                    <div class="div-btn">
+                        <button class="btn4" onclick="apriBox()">
+                            <div id="btn" style="display:block">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                                </svg>
+                            </div>
+                            <div id="btn2" style="display:  none">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                                </svg>
+                            </div>
+                        </button>
+                    </div>
+
                 </div>
                 <div id="box-card" style="display: none; ">
                     <div class="card-dash2">
@@ -95,15 +110,19 @@
                         </div>
                     </div>
                     <div style="flex:3">
-                        <div class="div-descr" style="">
+                        <div class="div-descr">
+                            <h6>User:</h6>
                             <h1>{{-- $announcement->title --}}</h1>
                             <h2>Prezzo : {{-- $announcement->price - € --}}</h2>
                             <p>Descrizione : {{-- $announcement->description --}}</p>
-                            <p>Categoria : {{-- $announcement->category->name --}}</p>
                         </div>
-                        <div>
-                            <button></button>
-                            <button></button>
+                        <div class="div_container_btn">
+                            <button class="btn_btn true">
+                                <h4>accetta</h4>
+                            </button>
+                            <button class="btn_btn false">
+                                <h4>rifiuta</h4>
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -119,12 +138,17 @@
 
             if (apertura === false) {
                 document.getElementById('box-card').style.display = "flex";
+                document.getElementById('btn').style.display = "none";
+                document.getElementById('btn2').style.display = "block";
+
                 apertura = true;
             } else {
                 document.getElementById('box-card').style.display = "none";
+                document.getElementById('btn').style.display = "block";
+                document.getElementById('btn2').style.display = "none";
                 apertura = false;
             }
         }
     </script>
 
-</x-layout>
+</x-layout-revisore>
