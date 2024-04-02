d2ac3c7c4ff144045c76b1ebb44eaa5d3833e1c4
fabio <fabio.forenza@gmail.com>
2024-04-02 17:49:26 +0200

    aggiornamento by-category

--- stiamo_freschi/resources/views/search-bycategory.blade.php
+++ stiamo_freschi/resources/views/search-bycategory.blade.php
@@ -8,12 +8,12 @@
         <h1>Ecco i risultati trovati per la categoria : {{-- {{$announcements->category->name}} --}} </h1>
     </div>
 
-    <div class="ContainerCard">
+    <div class="ContainerCard1">
         <div class="row">
             @foreach ($announcements as $announcement)
                 <div id="containerColCard" class="col-12 col-xl-3 col-lg-4 col-md-6 mt-5">
-                    <x-card-home :user="$announcement->user->name" :title="$announcement->title" :date="$announcement->updated_at->format('Y/m/d H:i:s')" :price="$announcement->price" :description="$announcement->description"
-                        :category="$announcement->category->name" :announcement="$announcement">
+                    <x-card-home :user="$announcement->user->name" :title="$announcement->title" :date="$announcement->updated_at->format('Y/m/d H:i:s')" :price="$announcement->price"
+                        :description="$announcement->description" :category="$announcement->category->name" :announcement="$announcement">
                     </x-card-home>
                 </div>
             @endforeach
