5e99de254664525f34a6652b9ed151a050845b85
fabio <fabio.forenza@gmail.com>
2024-04-07 20:19:39 +0200

    ora sono commentati

--- stiamo_freschi/resources/views/auth/register.blade.php
+++ stiamo_freschi/resources/views/auth/register.blade.php
@@ -56,7 +56,7 @@
                     <label class="form-check-label" for="exampleCheck1">{{ __('ui.rememberR') }}</label>
                 </div>
                 <button type="submit" class="BtnRegistratiLogin">{{ __('ui.registerR') }}</button>
-                <button type="submit" class="BtnRegistratiLogin">
+                {{-- <button type="submit" class="BtnRegistratiLogin">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                         class="bi bi-google" viewBox="0 0 16 16" style="margin-right: 20px">
                         <path
@@ -69,7 +69,7 @@
                         <path
                             d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                     </svg>
-                    Accedi con Google</button>
+                    Accedi con Facebook</button> --}}
 
 
                 <p class="giaRegistrato">{{ __('ui.requestRegister') }} <span style="font-weight: bold;"><a
