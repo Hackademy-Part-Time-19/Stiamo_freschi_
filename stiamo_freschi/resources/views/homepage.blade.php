c47686fcef10da4cfd0dd988e151638381f47c2c
Lleanza7 <lucaleanza92@hotmail.com>
2024-04-12 19:25:34 +0200

    fix home

--- stiamo_freschi/resources/views/homepage.blade.php
+++ stiamo_freschi/resources/views/homepage.blade.php
@@ -64,11 +64,7 @@
                     </div>
                 </div>
             </div>
-            {{--
-        <div class="grid-container" id="grid-container">
-            <!-- Le immagini verranno caricate qui dinamicamente -->
-        </div>
---}}
+
             @guest
                 <div id="pop" class="overlay">
                     <div class="popup">
@@ -79,74 +75,36 @@
                     </div>
                 </div>
             @endguest
-
-            {{-- @dd($announcements) --}}
-
-            <div class="ContainerCard" style="@if($announcements->isEmpty()) min-height: 20vh;@endif">
+            <div class="ContainerCard">
                 <div class="row" style="display:flex;justify-content:center;padding:0px;margin:0px;width:100%">
                     @forelse($announcements as $announcement)
                         <div id="containerColCard" class="col-12 col-xl-3 col-lg-4 col-md-6 mt-5">
                             @if ($announcement->images->isEmpty())
                                 <x-card-home :imagecard="asset('img/pexels-photo-4464487.jpeg')" :user="$announcement->user->name" :date="$announcement->updated_at->format('d/m/y H:i:s')" :title="$announcement->title"
-                                    :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->name" :announcement="$announcement">
+                                    :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->translatedName()" :announcement="$announcement">
                                 </x-card-home>
                             @else
                                 <x-card-home :imagecard="$announcement->images->first()->getUrl(200, 300)" :user="$announcement->user->name" :date="$announcement->updated_at->format('d/m/y H:i:s')" :title="$announcement->title"
-                                    :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->name" :announcement="$announcement">
+                                    :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->translatedName()" :announcement="$announcement">
                                 </x-card-home>
                             @endif
-
-<<<<<<< HEAD
-        <div class="ContainerCard">
-            <div class="row" style="display:flex;justify-content:center;padding:0px;margin:0px;width:100%">
-                @forelse($announcements as $announcement)
-                <div id="containerColCard" class="col-12 col-xl-3 col-lg-4 col-md-6 mt-5">
-                    @if ($announcement->images->isEmpty())
-                    <x-card-home :imagecard="asset('img/pexels-photo-4464487.jpeg')" :user="$announcement->user->name" :date="$announcement->updated_at->format('d/m/y H:i:s')" :title="$announcement->title"
-                        :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->translatedName()" :announcement="$announcement">
-                    </x-card-home>
-                    @else
-                    <x-card-home :imagecard="$announcement->images->first()->getUrl(200, 300)" :user="$announcement->user->name" :date="$announcement->updated_at->format('d/m/y H:i:s')" :title="$announcement->title"
-                        :price="$announcement->price" :description="$announcement->description" :category="$announcement->category->translatedName()" :announcement="$announcement">
-                    </x-card-home>
-                    @endif
-                </div>
-                @empty
-                <div class="col-12" style="display:flex;justify-content:center;align-items:center">
-                    <div class="alert alert-warning py-3 shadow">
-                        <p class="lead">{{ __('ui.alertNoAds') }}</p>
-                    </div>
-                </div>
-                @endforelse
-=======
                         </div>
-
                     @empty
-                        <div id="popSuccess" class="pop" x-data="{ isOpen: true }" x-init="setTimeout(() => { isOpen = false; }, 3000)">
-                            <div class="alert alert-warning traspWarning"
-                                style="width: 90%; display: flex;
-                                justify-content: center;align-items:center;text-align:center;font-size:20px"
-                                role="alert" x-show="isOpen" class="popup">
-                                {{ __('ui.alertNoAds') }}
-                                <div onclick="hidePopup()" class="btnCloseM"><svg xmlns="http://www.w3.org/2000/svg"
-                                        width="20" height="20" fill="black" class="bi bi-x-lg"
-                                        viewBox="0 0 16 16">
-                                        <path
-                                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                                    </svg></div>
+                        <div class="col-12" style="display:flex;justify-content:center;align-items:center">
+                            <div class="alert alert-warning py-3 shadow">
+                                <p class="lead">{{ __('ui.alertNoAds') }}</p>
                             </div>
                         </div>
                     @endforelse
-
-                    @if(!$announcements->isEmpty())
-                    <div class="containerPaginate">
-                            {{ $announcements->links() }}
-                    </div>
-                    @endif
-
                 </div>
->>>>>>> 1cad94b8bc8285757464a024ed45a63ee2d55d14
+                @if (!$announcements->isEmpty())
+                    <div class="containerPaginate">
+                        {{ $announcements->links() }}
+                    </div>
+                @endif
+
             </div>
+
         </div>
     </div>
     <script>
