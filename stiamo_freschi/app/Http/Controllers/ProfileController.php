5661245e133924ac295ca969c231ec92d2346bf1
Lleanza7 <lucaleanza92@hotmail.com>
2024-04-16 00:11:18 +0200

    fix reset password edit profile

--- stiamo_freschi/app/Http/Controllers/ProfileController.php
+++ stiamo_freschi/app/Http/Controllers/ProfileController.php
@@ -30,46 +30,54 @@ class ProfileController extends Controller
         return view('modifica-profilo');
     }
 
-public function update(EditProfile $request)
-{
-    $user = Auth::user();
-    $validateData = $request->validated();
+    public function update(EditProfile $request)
+    {
+        $user = Auth::user();
+        $validateData = $request->validated();
+        
+        // Verifica se la password corrente è stata fornita e se è corretta
+        if ($request->filled('current_password')) {
+            // Verifica che la password corrente fornita dall'utente sia corretta
+            if (!Hash::check($request->current_password, $user->password)) {
+                return redirect()->back()->withErrors(['current_password' => 'La password corrente non è corretta.'])->withInput();
+            }
+        }
     
-    // Verifica se la password corrente è stata fornita e se è corretta
-    if ($request->filled('current_password')) {
-        // Verifica che la password corrente fornita dall'utente sia corretta
-        if (!Hash::check($request->current_password, $user->password)) {
-            return redirect()->back()->withErrors(['current_password' => 'La password corrente non è corretta.'])->withInput();
+        // Se la nuova password è stata fornita e valida, aggiorna la password
+        if ($request->filled('password')) {
+            $validateData['password'] = Hash::make($validateData['password']);
+        } else {
+            // Se il campo password è vuoto, non modificarlo
+            unset($validateData['password']);
         }
-    }
-    // Se la nuova password è stata fornita e valida, aggiorna la password
-    if ($request->filled('password')) {
-        $validateData['password'] = Hash::make($validateData['password']);
-    }
-    // Aggiorna gli altri campi del profilo
-    $user->update($validateData);
-    // GESTIONE IMMAGINE
-    if ($request->hasFile('profile_image')) {
-        $file = $request->file('profile_image');
-        $file_name = $user->id . '.' . $file->getClientOriginalExtension();
-        $file->storeAs('public/profile_images', $file_name);
-        // Crea una nuova istanza di Image e salvala nel database
-        $image = new Image(['path' => $file_name]);
-        $image->save();
-
-        // Controlla se l'utente ha già un'immagine associata
-        if ($user->image) {
-            // Ottieni l'immagine associata all'utente
-            $old_image = $user->image;
-            // Rimuovi l'associazione tra l'utente e l'immagine
-            $user->image_id = null;
+    
+        // Aggiorna gli altri campi del profilo
+        $user->update($validateData);
+        
+        // GESTIONE IMMAGINE
+        if ($request->hasFile('profile_image')) {
+            $file = $request->file('profile_image');
+            $file_name = $user->id . '.' . $file->getClientOriginalExtension();
+            $file->storeAs('public/profile_images', $file_name);
+            
+            // Crea una nuova istanza di Image e salvala nel database
+            $image = new Image(['path' => $file_name]);
+            $image->save();
+    
+            // Controlla se l'utente ha già un'immagine associata
+            if ($user->image) {
+                // Ottieni l'immagine associata all'utente
+                $old_image = $user->image;
+                // Rimuovi l'associazione tra l'utente e l'immagine
+                $user->image_id = null;
+                $user->save();
+                $old_image->delete();
+            }
+            // Aggiorna l'ID dell'immagine per l'utente
+            $user->image_id = $image->id;
             $user->save();
-            $old_image->delete();
         }
-        // Aggiorna l'ID dell'immagine per l'utente
-        $user->image_id = $image->id;
-        $user->save();
+        return redirect()->back()->with('message', 'Profilo modificato con successo');
     }
-    return redirect()->back()->with('message', 'Profilo modificato con successo');
-}
+    
 }
